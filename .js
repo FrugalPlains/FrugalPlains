71e191dc0fb33de2d254cf22ed5fb217d1772b74
FrugalPlains <95050687+FrugalPlains@users.noreply.github.com>
2021-12-12 01:11:00 -0600

    Delete .js

--- .js
+++
@@ -1,9 +0,0 @@
-var firstName = 'Toni';
-let lastName = 'Tralice';
-const favoriteFood = 'Pizza';
-
-console.log(FirtName);
-console.log(lastName);
-console.log(favoriteFood);
-
-console.log(noDeclarada);
